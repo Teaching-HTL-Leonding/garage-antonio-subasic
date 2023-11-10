26b05b6640a0b601f65b178df6a01f93aae50bcc
Antonio Subašić <antonio.subasic.as@gmail.com>
2023-11-10 12:10:06 +0100

    add csharp/010 level1

---
+++ Garage.App/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+
+const int CAR_ENTRY = 1;
+const int CAR_EXIT = 2;
+const int REPORT = 3;
+const int EXIT = 4;
+
+var garage = new Garage.Logic.Garage();
+
+Console.WriteLine($@"What do you want to do?
+{CAR_ENTRY}) Enter a car entry
+{CAR_EXIT}) Enter a car exit
+{REPORT}) Generate report
+{EXIT}) Exit");
+
+while (true)
+{
+    var selection = GetInputFromUser<int>("\nYour selection");
+
+    var parkingSpotNumber = 0;
+    var exit = selection == EXIT;
+
+    if (selection is CAR_ENTRY or CAR_EXIT)
+    {
+        parkingSpotNumber = GetInputFromUser<int>("\nEnter parking spot number");
+        exit = exit || (garage.IsOccupied(parkingSpotNumber) ^ (selection != CAR_ENTRY));
+    }
+
+    switch (selection)
+    {
+        case CAR_ENTRY:
+            if (exit) { Console.WriteLine("Parking spot is occupied"); }
+            else
+            {
+                Console.Write($"Enter license plate: ");
+                var licensePlate = Console.ReadLine()!;
+                var entryDateTime = GetInputFromUser<DateTime>("Enter entry date/time");
+                garage.Occupy(parkingSpotNumber, licensePlate, entryDateTime);
+            }
+
+            break;
+
+        case CAR_EXIT:
+            if (exit) { Console.WriteLine("Parking spot is not occupied"); }
+            else
+            {
+                var exitDateTime = GetInputFromUser<DateTime>("Enter exit date/time");
+                Console.WriteLine($"Costs are {garage.Exit(parkingSpotNumber, exitDateTime)}€");
+            }
+
+            break;
+
+        case REPORT:
+            Console.WriteLine(garage);
+            break;
+
+        case EXIT:
+            Console.WriteLine("Good bye!");
+            return;
+    }
+}
+
+static T GetInputFromUser<T>(string message) where T : IParsable<T>
+{
+    Console.Write($"{message}: ");
+    var input = Console.ReadLine()!;
+    return T.Parse(input, CultureInfo.InvariantCulture);
+}
---
+++ Garage.Logic/Garage.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Garage.Logic;
+
+public record ParkingSpot(string LicensePlate, DateTime EntryDate);
+
+public class Garage
+{
+    public ParkingSpot?[] ParkingSpots { get; } = new ParkingSpot[50];
+
+
+    public bool IsOccupied(int parkingSpotNumber) => ParkingSpots[parkingSpotNumber - 1] != null;
+
+    public void Occupy(int parkingSpotNumber, string licensePlate, DateTime entryTime)
+    {
+        ParkingSpots[parkingSpotNumber - 1] = new(licensePlate, entryTime);
+    }
+
+    public decimal Exit(int parkingSpotNumber, DateTime exitTime)
+    {
+        var minutes = (exitTime - ParkingSpots[parkingSpotNumber - 1]!.EntryDate).TotalMinutes;
+        ParkingSpots[parkingSpotNumber - 1] = null;
+        return minutes < 15 ? 0 : (decimal)Math.Ceiling(minutes / 30) * 3;
+    }
+
+    public string GenerateReport()
+    {
+        var maxSpotLength = ParkingSpots.Length.ToString().Length;
+        var maxLicensePlateLength = ParkingSpots.Max(parkingSpot => parkingSpot?.LicensePlate.Length ?? 0);
+
+        var stringBuilder = new StringBuilder($"| {CenterString("Spot", maxSpotLength)} | {CenterString("License Plate", maxLicensePlateLength)} |");
+        stringBuilder.AppendLine($"\n| {new string('-', maxSpotLength)} | {new string('-', maxLicensePlateLength)} |");
+
+        for (var i = 0; i < ParkingSpots.Length; i++)
+        {
+            stringBuilder.AppendLine($"| {CenterString((i + 1).ToString(), maxSpotLength)} | {CenterString(ParkingSpots[i]?.LicensePlate, maxLicensePlateLength)} |");
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    public override string ToString() => GenerateReport();
+
+    private static string CenterString(string? str, int length)
+    {
+        var padding = Math.Max(length - (str?.Length ?? 0), 0) / 2d;
+
+        var before = new string(' ', (int)Math.Ceiling(padding));
+        var after = new string(' ', (int)padding);
+
+        return $"{before}{str?.Trim()}{after}";
+    }
+}
